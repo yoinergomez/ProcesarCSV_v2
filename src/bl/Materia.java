44b922ce549bc9ba0f252986403585e8f8282db2
yoiner.gomez <yoiner.gomez22@gmail.com>
2015-12-05 11:56:18 -0500

    Creación del modelo materia

---
+++ src/bl/Materia.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bl;
+
+/**
+ * 
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ * @version 1.1
+ */
+public class Materia {
+    
+    private String codigo;
+    private String departamento;
+    private String facultad;
+    private String nombre;
+
+    public Materia(String facultad, String departamento, String codigo, String nombre) {
+        this.codigo = codigo;
+        this.departamento = departamento;
+        this.facultad = facultad;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    
+}
+
