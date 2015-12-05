643c77f211a11a50aea9eb2d79a002278667d5e2
yoiner.gomez <yoiner.gomez22@gmail.com>
2015-12-05 14:35:55 -0500

    Creación del modelo GrupoProfesor

---
+++ src/bl/GrupoProfesor.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bl;
+
+/**
+ *
+ * @author esteban
+ */
+public class GrupoProfesor {
+    
+    private String materia;
+    private String departamento;
+    private String facultad;
+    private String cedula;
+    private String nombre;
+
+    public GrupoProfesor(Grupo grupo, String cedula, String nombre) {
+        this.materia = grupo.getMateria();
+        this.departamento = grupo.getDepartamento();
+        this.facultad = grupo.getFacultad();
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+  
+}
