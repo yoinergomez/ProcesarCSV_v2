cfc0af3ff524e970e66b396aa101bedd63101b7b
yoiner.gomez <yoiner.gomez22@gmail.com>
2015-12-06 11:50:24 -0500

    Creacion del modelo GrupoHorario

---
+++ src/bl/GrupoHorario.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bl;
+
+/**
+ *
+ * @author esteban
+ */
+public class GrupoHorario {
+    private String materia;
+    private String departamento;
+    private String facultad;
+    private String horario;
+
+    public GrupoHorario(Grupo grupo, String horario) {
+        this.materia = grupo.getMateria();
+        this.departamento = grupo.getDepartamento();
+        this.facultad = grupo.getFacultad();
+        this.horario = horario;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+    
+}
