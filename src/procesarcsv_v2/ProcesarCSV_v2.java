c40670bd94362b05865d9ee2b2520e9689bcef6a
yoiner.gomez <yoiner.gomez22@gmail.com>
2015-12-06 22:20:19 -0500

    Agrega el constructor de los insert SQL

--- src/procesarcsv_v2/ProcesarCSV_v2.java
+++ src/procesarcsv_v2/ProcesarCSV_v2.java
@@ -12,11 +12,16 @@ import bl.GrupoHorario;
 import bl.GrupoProfesor;
 import bl.Materia;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import javax.swing.JOptionPane;
 
@@ -26,6 +31,12 @@ import javax.swing.JOptionPane;
  */
 public class ProcesarCSV_v2 {
     
+    Map<String, Materia> materias = new HashMap<>();
+    ArrayList<Grupo> grupos = new ArrayList<>();
+    ArrayList<GrupoAula> aulas = new ArrayList<>();
+    ArrayList<GrupoHorario> horarios = new ArrayList<>();
+    ArrayList<GrupoProfesor> profesores = new ArrayList<>();
+    private StringBuilder sql = new StringBuilder();
     
     public void procesarlinea(){
         
@@ -37,15 +48,11 @@ public class ProcesarCSV_v2 {
         GrupoAula aula;
         GrupoHorario horario;
         GrupoProfesor profesor;
-        Map<String, Materia> materias = new HashMap<>();
-        ArrayList<Grupo> grupos = new ArrayList<>();
-        ArrayList<GrupoAula> aulas = new ArrayList<>();
-        ArrayList<GrupoHorario> horarios = new ArrayList<>();
-        ArrayList<GrupoProfesor> profesores = new ArrayList<>();
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     AQUI ESTABAN TODOS LOS ATRIBUTOS DE LA CLASE
         
         try {
-            //FileReader f = new FileReader("/home/esteban/Descargas/casosCSV.csv");
-            FileReader f = new FileReader("/home/esteban/Descargas/PROGRAMACION.csv");
+            FileReader f = new FileReader("/home/esteban/Descargas/casosCSV.csv");
+            //FileReader f = new FileReader("/home/esteban/Descargas/PROGRAMACION.csv");
             BufferedReader b = new BufferedReader(f);
             
             b.readLine();
@@ -128,17 +135,103 @@ public class ProcesarCSV_v2 {
             String mnj = "Error al leer el archivo ";
             JOptionPane.showMessageDialog(null, mnj+"\n"+ex , "Error",
             JOptionPane.WARNING_MESSAGE);
+        } catch (NullPointerException ex) {
+            String mnj = "La primera línea no contiene un curso válido";
+            JOptionPane.showMessageDialog(null, mnj+"\n"+ex , "Error",
+            JOptionPane.WARNING_MESSAGE);
         } 
         
     }
    
+    public void mostrarMaterias(){
+        System.out.println(materias.size());
+        sql.append("Materias "+materias.size()+"\n");
+        Iterator it = materias.keySet().iterator();
+        while(it.hasNext()){
+            String key = (String) it.next();
+            System.out.println(key + "\t" + materias.get(key).getNombre());
+            sql.append(key).append("\t").append(materias.get(key).getNombre()+"\n");
+        }
+    }
     
+    public void mostrarGrupos(){
+        System.out.println(grupos.size());
+        sql.append("Grupos "+grupos.size()+"\n");
+        for (int i = 0; i < grupos.size(); i++) {
+            System.out.println(grupos.get(i).getCodMateria()+
+                    "\t"+grupos.get(i).getNumero()+
+                    "\t"+grupos.get(i).getCupo()+
+                    "\t"+grupos.get(i).getMatriculados());
+            sql.append(grupos.get(i).getCodMateria()+
+                    "\t"+grupos.get(i).getNumero()+
+                    "\t"+grupos.get(i).getCupo()+
+                    "\t"+grupos.get(i).getMatriculados()+"\n");
+        }
+    }
+    
+    public void mostrarGrupoAula(){
+        System.out.println(aulas.size());
+        sql.append("GruposAulas "+aulas.size()+"\n");
+        for (int i = 0; i < aulas.size(); i++) {
+            System.out.println(aulas.get(i).getCodMateria()+
+                    "\t"+aulas.get(i).getAula());
+            sql.append(aulas.get(i).getCodMateria()+
+                    "\t"+aulas.get(i).getAula()+"\n");
+        }
+    }
+    
+    public void mostrarGrupoHorario(){
+        System.out.println(horarios.size());
+        sql.append("GruposHorarios "+horarios.size()+"\n");
+        for (int i = 0; i < horarios.size(); i++) {
+            System.out.println(horarios.get(i).getCodMateria()+
+                    "\t"+horarios.get(i).getHorario());
+            sql.append(horarios.get(i).getCodMateria()+
+                    "\t"+horarios.get(i).getHorario()+"\n");
+        }
+    }
+    
+    public void mostrarGrupoProfesor(){
+        System.out.println(profesores.size());
+        sql.append("GruposProfesores "+profesores.size()+"\n");
+        for (int i = 0; i < profesores.size(); i++) {
+            System.out.println(profesores.get(i).getCodMateria()+
+                    "\t"+profesores.get(i).getCedula()+
+                    "\t"+profesores.get(i).getNombre());
+            sql.append(profesores.get(i).getCodMateria()+
+                    "\t"+profesores.get(i).getCedula()+
+                    "\t"+profesores.get(i).getNombre()+"\n");
+        }
+    }
+    
+    public void generarSQL() {
+        String directorio = System.getProperty("user.dir") + "/PROGRAMACION.sql";
+        File file = new File(directorio);
+        try {
+            FileWriter w = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(w);
+            PrintWriter wr = new PrintWriter(bw);
+            wr.write(sql.toString());//Escribir en el archivo
+            wr.close();
+            bw.close();
+        } catch (IOException e) {
+
+        };
+
+    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ProcesarCSV_v2 run = new ProcesarCSV_v2();
+        run.procesarlinea();
+        run.mostrarMaterias();
+        run.mostrarGrupos();
+        run.mostrarGrupoAula();
+        run.mostrarGrupoHorario();
+        run.mostrarGrupoProfesor();
+        run.generarSQL();
     }
     
 }
