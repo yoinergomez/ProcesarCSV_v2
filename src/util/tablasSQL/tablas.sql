229b28d3fdecf596142685628ba6af4da759882b
yoiner.gomez <yoiner.gomez22@gmail.com>
2015-12-09 09:49:54 -0500

    Creacion de Grupo GrupoProfesor Profesor

--- src/util/tablasSQL/tablas.sql
+++ src/util/tablasSQL/tablas.sql
@@ -22,9 +22,44 @@ CREATE TABLE IF NOT EXISTS `Materia` (
 --
 
 CREATE TABLE IF NOT EXISTS `Grupo` (
-  `numero` tinyint(4) NOT NULL,
-  `materia` smallint(3) NOT NULL,
-  `cupo` tinyint(3) unsigned DEFAULT NULL,
-  `matriculados` tinyint(3) unsigned DEFAULT NULL,
-  PRIMARY KEY (`numero`,`materia`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `codigo` smallint(3) unsigned NOT NULL,
+  `departamento` smallint(2) unsigned NOT NULL,
+  `facultad` smallint(2) unsigned NOT NULL,
+  `numero` smallint(3) unsigned NOT NULL,
+  `cupo` smallint(3) unsigned DEFAULT NULL,
+  `matriculados` smallint(3) unsigned DEFAULT NULL,
+  PRIMARY KEY (`codigo`,`departamento`, `facultad`, `numero`),
+  FOREIGN KEY (`codigo`,`departamento`, `facultad`)
+      REFERENCES Materia(`codigo`,`departamento`, `facultad`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Profesor`
+--
+
+CREATE TABLE IF NOT EXISTS `Profesor` (
+  `cedula` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`cedula`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `GrupoProfesor`
+--
+
+CREATE TABLE IF NOT EXISTS `GrupoProfesor` (
+  `codigo` smallint(3) unsigned NOT NULL,
+  `departamento` smallint(2) unsigned NOT NULL,
+  `facultad` smallint(2) unsigned NOT NULL,
+  `numero` smallint(3) unsigned NOT NULL,
+  `cedula` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`,`departamento`, `facultad`, `numero`, `cedula`),
+  FOREIGN KEY (`codigo`,`departamento`, `facultad`, `numero`)
+      REFERENCES Grupo(`codigo`,`departamento`, `facultad`, `numero`),
+  FOREIGN KEY (`cedula`)
+      REFERENCES Profesor(`cedula`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
